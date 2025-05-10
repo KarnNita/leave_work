775e924204725c4615da377c72f09806be5a0ea4
Karnnitra Sukpanich <karnnitra.s@ku.th>
2025-05-11 00:26:59 +0700

    modify : 3 file

--- my-remix-app/vite.config.ts
+++ my-remix-app/vite.config.ts
@@ -1,12 +1,6 @@
-import { vitePlugin as remix } from "@remix-run/dev";
 import { defineConfig } from "vite";
+import { vitePlugin as remix } from "@remix-run/dev"; 
 import tsconfigPaths from "vite-tsconfig-paths";
-
-declare module "@remix-run/node" {
-  interface Future {
-    v3_singleFetch: true;
-  }
-}
 
 export default defineConfig({
   plugins: [
